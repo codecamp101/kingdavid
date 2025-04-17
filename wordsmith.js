5acf6aa1be91ecc9ae022d049ab3ee04eddeb9c2
codecamp101 <phildca114@gmail.com>
2025-04-17 14:52:45 +0100

    error_faw change

--- wordsmith.js
+++ wordsmith.js
@@ -14,3 +14,26 @@ function writeTxt () {
         
     }, 100);
 }
+let score = 0
+const checkBtn = document.querySelector('#checkBtn'); 
+checkBtn.addEventListener('click', () =>{
+console.log ("I have just been clicked");
+    document.querySelector('#cup > i').textContent = score;
+    score++;
+    document.querySelector('#action').style.backgroundImage = `url('error_fawzi_mourad.gif')`;
+    const tid = setTimeout(() => {
+        document.querySelector('#action').style.backgroundImage = 'none'
+        clearTimeout(tid);
+    }, 2000);
+});
+//STRING METHODS
+//.slice(start, end) //slices a portion ofa string
+//.concat(...strings)  //joins alll characters {chars} in a string
+//.trim()  //removes spaces at the beginning and end of a string
+//.at(..index) //chooses char at the index
+//.length //tells how many chars are in a string
+//.toLowerCase()  writes all chars in lower case
+//.UpperCase()  writes alll chars in upper case
+//.endsWith('')  //returns true if the char of the string matches the LAST char
+//startwith('')  //returns true if the char of the strings matches the FIRST char
+
